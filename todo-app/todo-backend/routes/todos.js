ff23a788d17eaf6155eb539fad7711c484d6bbfc
Michael Mroz <mroz15@hotmail.com>
2023-02-07 12:39:54 -0400

    implement and test a route to GET a single todo object

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,7 +35,8 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const singleTodo = await req.todo
+  res.send(singleTodo);
 });
 
 /* PUT todo. */
