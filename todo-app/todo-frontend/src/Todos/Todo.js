8085b431d2d872a33c9764f0f9969d49fca10bb6
Michael Mroz <mroz15@hotmail.com>
2023-02-09 17:13:27 -0400

    extract Todo obj into seperate component, rendered in List.js

---
+++ todo-app/todo-frontend/src/Todos/Todo.js
@@ -0,0 +1,33 @@
+const Todo = ({ todo, handleDelete, handleComplete }) => {
+
+    const doneInfo = (
+        <>
+          <span>This todo is done</span>
+          <span>
+            <button onClick={handleDelete(todo)}> Delete </button>
+          </span>
+        </>
+      )
+
+      const notDoneInfo = (
+        <>
+          <span>
+            This todo is not done
+          </span>
+          <span>
+            <button onClick={handleDelete(todo)}> Delete </button>
+            <button onClick={handleComplete(todo)}> Set as done </button>
+          </span>
+        </>
+      )
+    return (
+        <div style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '70%', margin: 'auto' }}>
+            <span>
+                {todo.text} 
+            </span>
+            {todo.done ? doneInfo : notDoneInfo}
+        </div>
+      )
+}
+
+export default Todo
